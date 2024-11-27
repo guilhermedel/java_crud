c73a8e7eba7cf4923640fdf89216388215c2e4b8
guilhermedel <glhrmgithub@gmail.com>
2024-11-27 00:36:38 -0300

    add mongodb

--- src/main/java/com/example/users/config/MongoConfig.java
+++ src/main/java/com/example/users/config/MongoConfig.java
@@ -10,6 +10,6 @@ public class MongoConfig {
 
     @Bean
     public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(new SimpleMongoClientDatabaseFactory("mongodb+srv://leodurao:Jsw6izm1Xb5fngTB@javacrud.ifcmh.mongodb.net/javadb?retryWrites=true&w=majority&appName=javaCrud"));
+        return new MongoTemplate(new SimpleMongoClientDatabaseFactory("mongodb+srv://leodurao:Jsw6izm1Xb5fngTB@javacrud.ifcmh.mongodb.net?retryWrites=true&w=majority&appName=javaCrud"));
     }
 }
